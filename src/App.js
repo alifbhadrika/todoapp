fa9f4654dd493db9cc1474f576c474e9a3e637dd
Alif Bhadrika Parikesit <alifbhadrika@gmail.com>
2021-08-04 14:48:48 +0700

    Revert "init"
    This reverts commit 201e838e4f46e59b76075721bca5a8f92b360b39.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,23 @@
-import "./App.css";
-import { useState } from "react";
-import Header from "./components/Header/Header";
-import Todolist from "./components/Todolist/Todolist";
-import Form from "./components/Form/Form";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [text, setText] = useState("");
-  const [todos, setTodos] = useState([]);
-  const [count, setCount] = useState(1);
-
-  const textHandler = (e) => {
-    setText(e.target.value);
-  };
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    setTodos([
-      ...todos,
-      {
-        id: count,
-        todo: text,
-        isCompleted: false,
-      },
-    ]);
-    setCount(count + 1);
-    setText("");
-  };
-  const completeToggle = (idx) => {
-    setTodos(
-      todos.map((todo, i) =>
-        i === idx
-          ? {
-              ...todo,
-              isCompleted: !todo.isCompleted,
-            }
-          : todo
-      )
-    );
-  };
   return (
     <div className="App">
-      <Header text="TODOs" />
-      <Todolist todos={todos} completeToggle={completeToggle} />
-      <Form
-        text={text}
-        textHandler={textHandler}
-        submitHandler={submitHandler}
-        count={count}
-      />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
